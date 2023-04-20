84552c78d92f80b93eceaaf7d54eed92c7a6c4c9
StrangeOnFire1 <131391144+StrangeOnFire1@users.noreply.github.com>
2023-04-20 22:24:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <h1 id='ayush'>Ayush</h1>
+   <div id="log-in">
+
+<h1 id="h1">Log-in</h1>
+<p id='p'>Log in to your account</p>
+<form action="">
+<label><p>  Username: <input type="text" name="username" id=""/>  </p></label>
+
+<label><p>   Password: <input type="text" name="password" id=""/> </p></label>
+
+       <p>  <input id='submit' type="submit" value="Log-in"/> </p>
+
+</form>
+
+<div class="or"></div>
+
+<div class="button">
+<span id="textbutton">If you are a new user,</span> <span id="button">Sign-up here</span> 
+<p>NOTE - The world today known is going to be ruled by the greatest demon lord,Lord Ayush</p>
+<h2>BOW BEFORE DEMON LORD AYUSH</h2>
+</div>
+
+</div>
+   
+   
+   </> 
+  );
+}
+
+export default App;
